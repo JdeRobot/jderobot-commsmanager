ad9a8725244b131b8ffa8cda3485c239060e5164
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-06-17 11:24:50 +0200

    Test webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,8 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    globalObject: 'this',
   },
   devtool: 'source-map'
 };
